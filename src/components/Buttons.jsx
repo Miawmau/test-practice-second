05fba0811830cf04a6d68074ae1621065efa504a
Mauri <maumenachotri224@gmail.com>
2024-10-18 23:30:22 -0300

    middle

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import imagen from "../assets/Vector.png"
+import "./Buttons.css"
+const Buttons = () => {
+  return (
+    <div className='buttons'>
+        <button>Follow</button>
+        <button>
+            <img src={imagen} alt="" />
+        </button>
+    </div>
+  )
+}
+
+export default Buttons
